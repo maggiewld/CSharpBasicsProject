497f911c1b42618ff68e25db255c563b7e84eb06
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-01-17 17:06:44 +0000

    Initialise project, comment in objectives.

---
+++ Treehouse.SimpleConsoleApp/Treehouse.SimpleConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehouse.SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt the user for the minutes exercised
+            //Add minutes exercised to total
+            //Display total minutes exercised to the screen
+            //Repeat until the user quits
+        }
+    }
+}
