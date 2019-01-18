a52c31cc8208efb63482dce59c4c78aaf15547a4
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-01-18 14:49:24 +0000

    Calculate runningTotal.

--- Treehouse.SimpleConsoleApp/Treehouse.SimpleConsoleApp/Program.cs
+++ Treehouse.SimpleConsoleApp/Treehouse.SimpleConsoleApp/Program.cs
@@ -7,14 +7,18 @@ namespace Treehouse.SimpleConsoleApp
     {
         static void Main(string[] args)
         {
+            int runningTotal = 0;
+            
             //Prompt the user for the minutes exercised
             Console.Write("Enter how many minutes you exercised: ");
             string entry = Console.ReadLine();
+            int minutes = int.Parse(entry);
 
+            runningTotal = runningTotal + minutes;
 
             //Add minutes exercised to total
             //Display total minutes exercised to the screen
-            Console.WriteLine("You've exercised " + entry + " minutes.");
+            Console.WriteLine("You've exercised " + runningTotal + " minutes.");
 
             //Repeat until the user quits
         }
